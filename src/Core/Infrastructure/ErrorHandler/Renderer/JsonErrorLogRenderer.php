4ced7c1c90a4639cd8da8706261ec2a610c3de03
Zoltan Borsos <zolli07@gmail.com>
2025-09-04 21:36:22 +0200

    feat[bootstrap]: Small improvements and removed a few test files

---
+++ src/Core/Infrastructure/ErrorHandler/Renderer/JsonErrorLogRenderer.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GardenManager\Api\Core\Infrastructure\ErrorHandler\Renderer;
+
+use GardenManager\Api\Core\Domain\Entity\Response\Enum\ResponseStatusEnum;
+use GardenManager\Api\Core\Infrastructure\Response\Builder\JsonResponseBuilder;
+use GardenManager\Api\Core\Infrastructure\Response\Factory\ResponseMetadataFactory;
+use Slim\Error\AbstractErrorRenderer;
+use \Throwable;
+
+class JsonErrorLogRenderer extends AbstractErrorRenderer
+{
+    public function __construct(
+        private readonly JsonResponseBuilder $responseBuilder,
+        private readonly ResponseMetadataFactory $responseMetadataFactory,
+    )
+    {
+    }
+
+    public function __invoke(Throwable $exception, bool $displayErrorDetails): string
+    {
+        return json_encode(
+            $this
+                ->responseBuilder
+                ->setStatus(ResponseStatusEnum::ERROR)
+                ->setData($exception->getMessage())
+                ->setException($exception)
+                ->addMetadata(
+                    $this->responseMetadataFactory->createExceptionMetadata($exception, $displayErrorDetails)
+                )
+                ->buildEntity(),
+            JSON_THROW_ON_ERROR
+        );
+    }
+}
